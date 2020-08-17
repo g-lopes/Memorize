87a9e69fa5514c8796ea08f8312284f6c13119ad
Guilherme Calabria Lopes <guicalabria@gmail.com>
2020-08-17 18:18:29 +0200

    End of first lecture

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,11 +8,50 @@
 
 import SwiftUI
 
+// A VIEW is just a rectangle on the screen.
 struct ContentView: View {
+    // The type of the property 'body' is ANY(some) type, as long as it behaves like a VIEW
+    // Notice that the var 'body' has no assignment symbol (=). Instead, it uses curly brackets.
+    // This means that this variable is not stored in memory. Instead, everytime the program needs it,
+    // the value of body needs to me calculate.
     var body: some View {
-        Text("Hello, World!")
+        // The next line is equivalent to: return Text("Hello, World!")
+        HStack {
+            ForEach(0..<4) { index in
+                CardView(isFaceUp: false)
+            }
+        }
+        .padding()
+        .foregroundColor(Color.green)
+        .font(Font.largeTitle)
     }
 }
+
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                            RoundedRectangle(cornerRadius: 20.0).fill(Color.white)
+                            RoundedRectangle(cornerRadius: 20.0).stroke(lineWidth: 5)
+                            Text("🥰")
+            } else {
+                RoundedRectangle(cornerRadius: 20.0).fill()
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
